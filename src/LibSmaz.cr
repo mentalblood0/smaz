6f99b2cf2b614b6bf5c66cffbb98e187124b6b2b
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-08-11 18:22:57 +0300

    more accurate typing

--- src/LibSmaz.cr
+++ src/LibSmaz.cr
@@ -1,5 +1,5 @@
 @[Link(ldflags: "#{__DIR__}/smaz/smaz.o")]
 lib LibSmaz
-  fun compress = smaz_compress(in : LibC::Char*, len : LibC::SizeT, out : LibC::Char*, bufsize : LibC::SizeT) : LibC::SizeT
-  fun decompress = smaz_decompress(in : LibC::Char*, len : LibC::SizeT, out : LibC::Char*, bufsize : LibC::SizeT) : LibC::SizeT
+  fun compress = smaz_compress(in : LibC::Char*, inlen : LibC::Int, out : LibC::Char*, outlen : LibC::Int) : LibC::Int
+  fun decompress = smaz_decompress(in : LibC::Char*, inlen : LibC::Int, out : LibC::Char*, outlen : LibC::Int) : LibC::Int
 end
